ef42012c143c7f656765ee2c1d3113c0f9539ff3
teckila2db <arthur.perciballi@outlook.com>
2018-04-17 15:14:36 +0200

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp5/Snake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp5
+{
+    public class Snake
+    {
+        public Rectangle[] snakeRec;
+        private SolidBrush brush;
+    }
+}
