24700b375a4b7dc081b6d1d05f474b86973c5a04
teckila2db <arthur.perciballi@outlook.com>
2018-04-19 15:55:11 +0200

    Add files via upload

---
+++ Snake-master/WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.snakeScoreLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.spaceBarLabel = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.snakeScoreLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 423);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(477, 25);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(127, 20);
+            this.toolStripStatusLabel1.Text = "Ton score est de : ";
+            // 
+            // snakeScoreLabel
+            // 
+            this.snakeScoreLabel.Name = "snakeScoreLabel";
+            this.snakeScoreLabel.Size = new System.Drawing.Size(17, 20);
+            this.snakeScoreLabel.Text = "0";
+            // 
+            // spaceBarLabel
+            // 
+            this.spaceBarLabel.AutoSize = true;
+            this.spaceBarLabel.Location = new System.Drawing.Point(88, 204);
+            this.spaceBarLabel.Name = "spaceBarLabel";
+            this.spaceBarLabel.Size = new System.Drawing.Size(295, 17);
+            this.spaceBarLabel.TabIndex = 1;
+            this.spaceBarLabel.Text = "Appuis sur la barre espace pour commencer !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 448);
+            this.Controls.Add(this.spaceBarLabel);
+            this.Controls.Add(this.statusStrip1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel snakeScoreLabel;
+        private System.Windows.Forms.Label spaceBarLabel;
+    }
+}
+
---
+++ Snake-master/WindowsFormsApp5/WindowsFormsApp5/Snake.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp5
+{
+    public class Snake
+    {
+        public Rectangle[] snakeRec;
+        public SolidBrush brush;
+        public int x, y, width, height;
+
+        public Rectangle[] SnakeRec
+        {
+            get { return snakeRec; }
+        }
+
+
+        public Snake()
+        {
+            snakeRec = new Rectangle[3];
+            brush = new SolidBrush(Color.DarkBlue);
+
+            x = 20;
+            y = 0;
+            width = 10;
+            height = 10;
+
+            for (int i = 0; i < snakeRec.Length; i++)
+            {
+                snakeRec[i] = new Rectangle(x, y, width, height);
+                x -= 10;
+            }
+        }
+        public void drawSnake(Graphics paper)
+        {
+            foreach (Rectangle rec in snakeRec)
+            {
+                paper.FillRectangle(brush, rec);
+            }
+        }
+
+        public void drawSnake()
+        {
+            for(int i = snakeRec.Length - 1;  i > 0; i--)
+            {
+                snakeRec[i] = snakeRec[i - 1];
+            }
+        }
+
+        public void moveDown()
+        {
+            drawSnake();
+            snakeRec[0].Y += 10;
+        }
+        public void moveUp()
+        {
+            drawSnake();
+            snakeRec[0].Y -= 10;
+        }
+        public void moveRight()
+        {
+            drawSnake();
+            snakeRec[0].X += 10;
+        }
+        public void moveLeft()
+        {
+            drawSnake();
+            snakeRec[0].X -= 10;
+        }
+
+        public void growSnake()
+        {
+            List<Rectangle> rec = snakeRec.ToList();
+            rec.Add(new Rectangle(snakeRec[snakeRec.Length - 1].X, snakeRec[SnakeRec.Length - 1].Y, width, height));
+            snakeRec = rec.ToArray();
+        }
+    }
+}
